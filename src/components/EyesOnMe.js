a361077ee807c10b1deb61c82a39729c83acb20c
Seth Mwebi <sethmwebi27@gmail.com>
2024-08-05 10:10:32 +0300

    added code on react handling form

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,11 @@
 // Code EyesOnMe Component Here
+const EyesOnMe = () => (
+  <button
+    onFocus={() => console.log("Good!")}
+    onBlur={() => console.log("Hey! Eyes on me!")}
+  >
+    Eyes on me
+  </button>
+);
+
+export default EyesOnMe;
